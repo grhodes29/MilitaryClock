52565e3ec68c302ef6a242f3de7abc231ed5f628
grhodes29 <grhodes29@gmail.com>
2019-02-19 13:20:36 -0600

    finished, working, but display not to spec yet

--- MilitaryClock/Program.cs
+++ MilitaryClock/Program.cs
@@ -1,15 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MilitaryClock
+﻿namespace MilitaryClock
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
     class Program
     {
         static void Main(string[] args)
         {
+
+            //Write a console application that:
+
+            //Accurately counts from midnight to midnight using a standard 24hr military format.
+            //The clock should be accurate in the way it counts: No skipping numbers; Not displaying 60; etc. 
+
+            //Finished Product should:
+
+            //Not display old times   -- Console.Clear(); ????
+            //Allow 1 second to pass between each display // Timer class maybe so we can track 1 second increments
+            //Should not use DateTime in any way!!! System.DateTime
+            //The clock should start at 00:00:00 when the application starts. 
+
+           
+            Timer _t = new Timer();
+            
+            // seconds which will be 00,01,02 ... 59 and then back to 00
+
+            // minutes
+            // each time we complete a loop of the second we want to increment the minutes
+            // 01,02 ... 59
+            // minutes and seconds are very much alike
+
+            // hours are slightly different
+            /// 01,02 .. 23 only
+            /// 
+
+            // stop value with be 23:59:59
+
+
+            // could do this whole thing as three loops ???
+
+            // hours
+            for (int _hours = 0; _hours < 24; _hours++)
+            {
+                // minutes
+                for (int _minutes = 0; _minutes < 60; _minutes++)
+                {
+                    // seconds
+                    for (int _seconds = 0; _seconds < 60; _seconds++)
+                    {
+                        // this where we will sleep for the one second
+                        _t.IncrementOneSecond(_hours, _minutes, _seconds);
+
+                        // update timer done internally in Timer class private method
+                                                      
+                        // update the display
+                            Console.Write(" {0}:{1}:{2}", _t.HoursDisplay, _t.MinutesDisplay, _t.SecondsDisplay);
+                    }
+                }
+            }
+
         }
     }
 }
---
+++ MilitaryClock/Timer.cs
@@ -0,0 +1,77 @@
+﻿namespace MilitaryClock
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// DESCRIPTION: 
+    /// </summary>
+    public class Timer
+    {
+        private const int _milliseconds = 1000;
+
+        public string SecondsDisplay { get; set; }
+        public string MinutesDisplay { get; set; }
+        public string HoursDisplay { get; set; }
+        // total seconds is (23 * 60 *60) = 82800 seconds + (59 * 60) 3540 seconds + 59 seconds = 86399
+        private int TotalElapseTimeInSeconds { get; set; }
+        private int Seconds { get; set; }
+        private int Minutes { get; set; }
+        private int Hours { get; set; }
+
+        private Stopwatch Watch { get; set; }
+
+
+        public Timer() {
+
+            Watch = new Stopwatch();
+            TotalElapseTimeInSeconds = 0;
+            this.Hours = 0;
+            this.Minutes = 0;
+            this.Seconds = 0;
+            this.SecondsDisplay = "00";
+            this.MinutesDisplay = "00";
+            this.HoursDisplay = "00";
+        }
+
+
+        public void IncrementOneSecond(int inHrs, int inMinutes, int inSeconds) {
+
+            // so technically, 1000 milliseconds = 1 secs so make sure you set it 
+            // correctly here before handing it in
+            Watch.Start();
+            Thread.Sleep(_milliseconds);
+            Watch.Stop();
+
+            // this should update all the string properties for the display
+            UpdateTimer(inHrs, inMinutes, inSeconds);
+
+        }
+
+
+        private void UpdateTimer(int inHrs, int inMins, int inSecs)
+        {
+            // write some pseudocode here
+            this.Seconds = inSecs;
+            this.Minutes = inMins;
+            this.Hours = inHrs;
+            this.TotalElapseTimeInSeconds++;
+            ConvertIntTimeToStringTime();
+
+        }
+
+        private void ConvertIntTimeToStringTime() {
+
+            this.HoursDisplay = Hours.ToString("D2");
+            this.MinutesDisplay = Minutes.ToString("D2");
+            this.SecondsDisplay = Seconds.ToString("D2");
+
+        }
+
+    }
+}
